a7578c083e18ebeb5a33921ca15857dc463be097
mojhaid <mojahid319@gmail.com>
2022-06-08 07:42:19 +0600

    add reset password

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react';
 import './Login.css';
-import { Button, Form } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
@@ -51,7 +50,7 @@ if(gloading || loading){
      <div id="wraper">
        <div className="from-container">
          <span className="heading">Login</span>
-         <form action="">
+         <form action="" onSubmit={Handalformsubmit}>
            <div className="input-group">
              <input type="email" placeholder='Email' />
              <span className="bar"></span>
@@ -61,37 +60,28 @@ if(gloading || loading){
              <span className="bar"></span>
            </div>
            <div className="input-group">
-             <button>login</button>
+             <button>Submit</button>
+             
+           </div>
+           <div className="swich-logn">
+           <p className='text-center'>Are you New ? <Link to='/signUp' onClick={handalsignUp} className="text-danger mt-3 text-decoration-none">Sign Up</Link> </p>
+             
+           </div>
+           <div className='or'>
+             <p>or</p>
+           </div>
+           <div className="input-group">
+             <button onClick={() => signInWithGoogle()}>
+               Sign In With Google
+             </button>
+             
            </div>
          </form>
        </div>
      </div>
-       <div className='w-25 mx-auto mt-5'>
-         <h1 className='text-center mt-5'>Plese Login</h1>
-       <Form  onSubmit={Handalformsubmit}>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    
-    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
-    
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-  
-    <Form.Control ref={passRef} type="password" placeholder="Password" />
-  </Form.Group>
- 
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-       </div>
-
-       <p className='text-center my-4'>Or</p>
-       <p className='text-center'>are you New ?<Link to='/signUp' onClick={handalsignUp} className="text-danger mt-3 text-decoration-none">Plese Sign Up</Link> </p>
        
-    <div className='text-center mt-5'>
-       <button onClick={() => signInWithGoogle()} className='btn btn-outline-dark '>Sign In with google</button>
-    </div>
+
+      
      </>
   );
 };
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,5 +1,5 @@
+import { sendPasswordResetEmail } from 'firebase/auth';
 import React, { useRef } from 'react';
-import { Button, Form } from 'react-bootstrap';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
@@ -14,7 +14,7 @@ const SignUp = () => {
         createUserWithEmailAndPassword,
         user
         
-      ] = useCreateUserWithEmailAndPassword(auth);
+      ] = useCreateUserWithEmailAndPassword(auth,{sendEmailVerification:true});
       
     
     const handallogin= ()=>{
@@ -22,6 +22,12 @@ const SignUp = () => {
     }
     if(user){
         navigate('/');
+    }
+    const handalforget=()=>{
+      sendPasswordResetEmail(auth)
+      .then(()=>{
+        console.log('email send')
+      })
     }
     const handalsignup= event =>{
         event.preventDefault();
@@ -35,32 +41,40 @@ const SignUp = () => {
 
     return (
         <div>
-        <h3 className='text-center my-4'>Sign Up</h3>
-            <Form onSubmit={handalsignup} className='w-25 mx-auto'>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-   
-    <Form.Control ref={namelRef} type="name" placeholder="Name" />
-    
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-   
-    <Form.Control ref={emailRef} type="email" placeholder="Enter email" />
-    
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-   
-    <Form.Control ref={PasswordRef} type="password" placeholder="Password" />
-  </Form.Group>
-
-
- 
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-<p className="mt-3 text-center">All ready have an account ? <Link to='/login' onClick={handallogin} className="text-danger mt-3 text-decoration-none">Plese Login</Link> </p>
+           <div id="wraper">
+       <div className="from-container">
+         <span className="heading">Sign Up</span>
+         <form action="" onSubmit={handalsignup}>
+           <div className="input-group">
+             <input type="text" placeholder='Name' />
+             <span className="bar"></span>
+           </div>
+           <div className="input-group">
+             <input type="email" placeholder='Email' />
+             <span className="bar"></span>
+           </div>
+           <div className="input-group">
+             <input type="password" placeholder='password' />
+             <span className="bar"></span>
+           </div>
+           <div className="input-group">
+             <button>Submit</button>
+             
+           </div>
+           <div className="swich-logn">
+           <p className='text-center'>All ready have an account  ? <Link to='/login' onClick={handallogin} className="text-danger mt-3 text-decoration-none">Login</Link> </p>
+             
+           </div>
+           <div className="swich-logn">
+           <p className='text-center text-decoration-none'> <Link to=''>foget password ?</Link></p>
+             
+           </div>
+          
+        
+         </form>
+       </div>
+     </div>
+       
         </div>
     );
 };
